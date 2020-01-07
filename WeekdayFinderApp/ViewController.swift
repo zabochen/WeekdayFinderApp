e0517c424f944de837c18152beab9f46d327cf84
Taras <zabochen@gmail.com>
2020-01-07 10:52:31 +0200

    Edit "viewController"

--- WeekdayFinderApp/ViewController.swift
+++ WeekdayFinderApp/ViewController.swift
@@ -2,19 +2,59 @@
 //  ViewController.swift
 //  WeekdayFinderApp
 //
-//  Created by Taras on 04.01.2020.
-//  Copyright © 2020 Taras. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        dateTextField.tintColor = .blue
+        monthTextField.tintColor = .blue
+        yearTextField.tintColor = .blue
     }
-
-
+    
+    
+    // MARK: Find Day Button
+    @IBAction func findDay() {
+        
+        let currentCalendar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        
+        guard
+            let day = dateTextField.text,
+            let month = monthTextField.text,
+            let year = yearTextField.text
+            else { return }
+        
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard
+            let date = currentCalendar.date(from: dateComponents)
+            else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        // List of locale
+        // https://gist.github.com/jacobbubu/1836273
+        dateFormatter.locale = Locale(identifier: "uk_UA")
+        
+        let weekDay = dateFormatter.string(from: date)
+        self.resultLabel.text = weekDay.capitalized
+        
+    }
+    
+    // MARK: Hide Keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
-
